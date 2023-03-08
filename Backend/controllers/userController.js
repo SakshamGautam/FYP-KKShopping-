d18ee7fb881fde2eca31bf9d776497f075703c2e
SakshamGautam <np01cp4s210007@islingtoncollege.edu.np>
2023-03-08 15:16:29 +0545

    backend user/password auth (incomplete)

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,58 @@
+const ErrorHandler = require("../utils/errorhandler");
+const catchAsyncErrors = require("../middleware/catchAsyncError");
+const User = require("../models/userModel");
+const sendToken = require("../utils/jwtToken");
+
+//Register User
+exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+  const { name, email, password } = req.body;
+  const user = await User.create({
+    name,
+    email,
+    password,
+    avatar: {
+      public_id: "this is a sample ID",
+      url: "profilepicURL",
+    },
+  });
+
+  sendToken(user, 201, res);
+});
+
+//login user
+exports.loginuser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //checking if the user has given both the email and password
+
+  if (!email || !password) {
+    return next(new ErrorHandler("Please Enter Email and Password", 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
+
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
+
+  sendToken(user, 200, res);
+});
+
+//logout user
+exports.logout = catchAsyncErrors(async (req, res, next) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Logged Out",
+  });
+});
--- Backend/routes/productRoute.js
+++ Backend/routes/productRoute.js
@@ -6,17 +6,24 @@ const {
   updateProduct,
   deleteProduct,
 } = require("../controllers/productController");
+const { isAuthenticatedUser, authoriseRoles } = require("../middleware/autho");
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
 
-router.route("/product/new").post(createProduct);
+router
+  .route("/product/new")
+  .post(isAuthenticatedUser, authoriseRoles("admin"), createProduct);
 
 router.route("/product/:id").get(getProductDetails);
 
-router.route("/product/:id").put(updateProduct);
+router
+  .route("/product/:id")
+  .put(isAuthenticatedUser, authoriseRoles("admin"), updateProduct);
 
-router.route("/product/:id").delete(deleteProduct);
+router
+  .route("/product/:id")
+  .delete(isAuthenticatedUser, authoriseRoles("admin"), deleteProduct);
 
 module.exports = router;
